50e4a94d483e3d3c81eb65abf297f812f65c7d71
mashashtiv <mashashtiv@gmail.com>
2024-07-22 18:33:13 +0300

    modified driver

--- examples/draft.py
+++ examples/draft.py
@@ -12,20 +12,6 @@ def get_lane_range(starting_position):
     else:
         # Default to a range that includes only the starting position if not 1 or 4
         return starting_position, starting_position
-def penguin(cur_pos_x, cur_pos_y, world, max_x):
-    for next in range(-1,1):
-        next_obs = world.get((cur_pos_x - next, cur_pos_y - 1))
-        if next_obs == obstacles.PENGUIN:
-            match next:
-                case -1:
-                    if cur_pos_x +1 != max_x:
-                        return actions.RIGHT
-                case 0:
-                    return actions.PICKUP
-                case 1:
-                    return actions.LEFT
-        else:
-            return actions.NONE
 
 def run(cur_pos_x, cur_pos_y, obs, world, min_x, max_x):
     if cur_pos_x == min_x:
@@ -59,28 +45,18 @@ def drive(world):
     obs = world.get((cur_pos_x, cur_pos_y - 1))
     match obs:
         case obstacles.NONE:
-            penguin(cur_pos_x, cur_pos_y, world, max_x)
             return actions.NONE
         case obstacles.PENGUIN:
             return actions.PICKUP
         case obstacles.WATER:
-            penguin(cur_pos_x, cur_pos_y, world, max_x)
             return actions.BRAKE
         case obstacles.CRACK:
-            penguin(cur_pos_x, cur_pos_y, world, max_x)
             return actions.JUMP
         case obstacles.TRASH:
-            penguin(cur_pos_x, cur_pos_y, world, max_x)
             return run(cur_pos_x, cur_pos_y, obs, world, min_x, max_x)
         case obstacles.BIKE:
-            penguin(cur_pos_x, cur_pos_y, world, max_x)
             return run(cur_pos_x, cur_pos_y, obs, world, min_x, max_x)
         case obstacles.BARRIER:
-            penguin(cur_pos_x, cur_pos_y, world, max_x)
             return run(cur_pos_x, cur_pos_y, obs, world, min_x, max_x)
         case _:
             return actions.NONE
-
-
-
-
